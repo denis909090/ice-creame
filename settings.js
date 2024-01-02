8358829306844385a934c3a63383b35ad5a6a8b5
denis909090 <135863448+denis909090@users.noreply.github.com>
2024-01-02 22:05:53 +0200

    jhgjh

--- settings.js
+++ settings.js
@@ -1,47 +1,28 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-    };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-    }
-})
-
-const cmdTop = document.querySelector('.scroll-to-top');
+const cmdTop = document.querySelector(".scroll-to-top");
 
 if (cmdTop) {
-  cmdTop.addEventListener ('click', click, false);
-  cmdTop.addEventListener ('touchend', click, false);
+  cmdTop.addEventListener("click", click, false);
+  cmdTop.addEventListener("touchend", click, false);
 }
 
 function click(event) {
   event.preventDefault();
 
-    const targetId = this.getAttribute("href");
-    const targetElement = document.querySelector(targetId);
-    
-    if (targetElement) { 
-      targetElement.scrollIntoView({ behavior: "smooth", });
-    }
+  const targetId = this.getAttribute("href");
+  const targetElement = document.querySelector(targetId);
+
+  if (targetElement) {
+    targetElement.scrollIntoView({ behavior: "smooth" });
+  }
 }
 
-
-document.addEventListener('scroll', function(){
+document.addEventListener("scroll", function () {
   if (!cmdTop) {
-
-    return
+    return;
   }
 
-  if (window.scrollY>200) {
-      cmdTop.classList.add('show');
-    }
-    else (
-      cmdTop.classList.remove('show')
-    )
-})
+  if (window.scrollY > 100) {
+    cmdTop.classList.add("show");
+  }
+  else cmdTop.classList.remove("show");
+});
