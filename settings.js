3254e779fc5a774367d9db63c8337b8333e276b9
denis909090 <135863448+denis909090@users.noreply.github.com>
2024-01-02 02:18:20 +0200

    asasd

--- settings.js
+++ settings.js
@@ -11,4 +11,34 @@
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
     }
-})();
+})
+
+const cmdTop = document.querySelector('.scroll-to-top');
+
+if (cmdTop) {
+  cmdTop.addEventListener ('click', function(event){
+    event.preventDefault();
+
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+    
+    if (targetElement) { 
+      targetElement.scrollIntoView({ behavior: "smooth", });
+    }
+  })
+}
+
+
+document.addEventListener('scroll', function(){
+  if (!cmdTop) {
+
+    return
+  }
+
+  if (window.scrollY>200) {
+      cmdTop.classList.add('show');
+    }
+    else (
+      cmdTop.classList.remove('show')
+    )
+})
